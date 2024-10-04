59f5891a0a8c34ad415cf6214253dfbd6e1abde6
Alex23603 <alexx24603@gmail.com>
2024-10-04 21:30:54 +0000

    5-building.js

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,13 +1,12 @@
 import Building from './5-building.js';
 
 const b = new Building(100);
-console.log(b);
+console.log(b); // Outputs: Building { _sqft: 100 }
 
 class TestBuilding extends Building {}
 
 try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
+    new TestBuilding(200);
+} catch (err) {
+    console.log(err); // Outputs: Error: Class extending Building must override evacuationWarningMessage
 }
